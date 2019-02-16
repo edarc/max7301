bc95eb79165362044b989834819ea60a7fc16503
Kyle Schaffrick <kyle@raidi.us>
2019-02-16 00:49:56 -0800

    Set up empty crate.

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Driver library for the Maxim MAX7301 serial I/O expander.
+//!
+//! This driver is intended to work on embedded platforms using any implementation of the
+//! `embedded-hal` trait library.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "std")]
+extern crate core;
+
+extern crate embedded_hal as hal;
